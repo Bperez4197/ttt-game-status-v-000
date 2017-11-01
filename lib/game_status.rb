1830f2cabe3f4b024d92dbe6448bd080e7cde84a
Bperez4197 <bryceperez31@gmail.com>
2017-11-01 13:32:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,7 +30,7 @@ end
 def full?(board)
   if board.all? do |full_board| full_board == "X" || "O"
     return true
-  elsif board.all? != "X" || "O"
+  else board.all? != "X" || "O"
     return false
     end
   end
