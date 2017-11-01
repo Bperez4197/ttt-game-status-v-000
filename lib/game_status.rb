cc4fb223a5c591a20af390cf645e038e400a3a8b
Bperez4197 <bryceperez31@gmail.com>
2017-11-01 13:13:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,33 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+def won?(board)
+  combo = 0
+  while combo < WIN_COMBINATIONS.length
+    current_combo = WIN_COMBINATIONS[combo]
+    win1 = current_combo.all? {|position| board[position] == "X"}
+    win2 = current_combo.all? {|position| board[position] == "O"}
+  if win1 || win2
+      return current_combo
+    else
+    end
+    combo += 1
+  end
+end
+def full?(full_board)
+  if board.any? do |available| available == " "
+    return false
+  else
+  end
+end
+end
